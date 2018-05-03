da37290d21c68700be499d272cb1cc70f8331741
Maurice Kraus <maurice.kraus@stud.tu-darmstadt.de>
2018-05-03 21:19:16 +0200

    fixed constant bug

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,7 +1,7 @@
 export const REQUEST_TODO = 'REQUEST_TODO';
 export const REQUEST_TODOS = 'REQUEST_TODOS';
 export const RECEIVE_TODOS = 'RECEIVE_TODOS';
-export const RECEIVE_TODO = 'REQUEST_TODO';
+export const RECEIVE_TODO = 'RECEIVE_TODO';
 
 export const ADD_TODO = 'ADD_TODO';
 export const GET_TODOS = 'GET_TODOS';
