3bb6fe81f860e645255dfb313a731975013a0655
Maurice Kraus <maurice.kraus@stud.tu-darmstadt.de>
2018-05-05 21:42:38 +0200

    removed proptype validation and added flow validation

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,57 +1,77 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Paper } from 'material-ui';
-import MaterialList from 'material-ui/List';
-import { connect } from 'react-redux';
-import { formValueSelector, clearFields, change } from 'redux-form';
+import * as React from "react";
+import { Paper } from "material-ui";
+import MaterialList from "material-ui/List";
+import { connect } from "react-redux";
+import { formValueSelector, clearFields, change } from "redux-form";
+
 import {
   fetchTodos,
   addTodo,
   toggleTodo,
   deleteTodo,
   changeEditMode,
-  updateTodo,
-} from '../actions';
-import CheckItem from './checkItem/CheckItem';
-import AddFormContainer from './addForm/AddFormContainer';
-import './list.css';
+  updateTodo
+} from "../actions";
+import CheckItem from "./checkItem/CheckItem";
+import AddFormContainer from "./addForm/AddFormContainer";
+import "./list.css";
 
-class List extends Component {
+type Props = {
+  todos: Array<Todo>,
+  editMode: {
+    mode: boolean,
+    todo: Todo
+  },
+  textField: string,
+  fetchTodos: () => void,
+  updateTodo: (Todo, string) => void,
+  clearFields: (string, boolean, boolean, string) => void,
+  addTodo: string => void,
+  toggleTodo: Todo => void,
+  deleteTodo: Todo => void,
+  changeEditMode: Todo => void,
+  change: (string, string, string) => void
+};
+
+class List extends React.Component<Props, {}> {
   componentWillMount() {
     this.props.fetchTodos();
   }
-  submit = () => {
+  submit = (): void => {
     if (this.props.editMode.mode) {
       this.props.updateTodo(this.props.editMode.todo, this.props.textField);
-      this.props.clearFields('addForm', false, false, 'todoTitle');
+      this.props.clearFields("addForm", false, false, "todoTitle");
     } else if (this.props.textField !== undefined) {
       if (this.props.textField.trim().length !== 0) {
         this.props.addTodo(this.props.textField);
       }
-      this.props.clearFields('addForm', false, false, 'todoTitle');
+      this.props.clearFields("addForm", false, false, "todoTitle");
     }
   };
 
-  handleRowClick = (todo) => {
+  handleRowClick = (todo: Todo): void => {
     this.props.toggleTodo(todo);
   };
 
-  handleDeleteRowClick = (todo) => {
+  handleDeleteRowClick = (todo: Todo): void => {
     this.props.deleteTodo(todo);
-    this.props.clearFields('addForm', false, false, 'todoTitle');
+    this.props.clearFields("addForm", false, false, "todoTitle");
   };
 
-  handelEditRowClick = (todo) => {
-    this.props.change('addForm', 'todoTitle', todo.title);
+  handelEditRowClick = (todo: Todo): void => {
+    this.props.change("addForm", "todoTitle", todo.title);
     this.props.changeEditMode(todo);
   };
 
   render() {
     return (
       <Paper>
-        <AddFormContainer onPress={this.submit} editMode={this.props.editMode.mode} />
+        <AddFormContainer
+          onPress={this.submit}
+          editMode={this.props.editMode.mode}
+        />
         <MaterialList className="paper-container">
-          {this.props.todos.map(todo => (
+          {this.props.todos.map((todo: Todo) => (
             <div key={todo._id}>
               <CheckItem
                 title={todo.title}
@@ -68,13 +88,13 @@ class List extends Component {
   }
 }
 
-const mapStateToProps = (state) => {
-  const selector = formValueSelector('addForm');
+const mapStateToProps = (state: State) => {
+  const selector = formValueSelector("addForm");
   return {
     todos: state.todos.todos,
     isFetching: state.todos.isFetching,
     editMode: state.todos.editMode,
-    textField: selector(state, 'todoTitle'),
+    textField: selector(state, "todoTitle")
   };
 };
 export default connect(mapStateToProps, {
@@ -85,30 +105,5 @@ export default connect(mapStateToProps, {
   clearFields,
   change,
   changeEditMode,
-  updateTodo,
+  updateTodo
 })(List);
-
-List.propTypes = {
-  clearFields: PropTypes.func.isRequired,
-  fetchTodos: PropTypes.func.isRequired,
-  deleteTodo: PropTypes.func.isRequired,
-  updateTodo: PropTypes.func.isRequired,
-  changeEditMode: PropTypes.func.isRequired,
-  change: PropTypes.func.isRequired,
-  addTodo: PropTypes.func.isRequired,
-  toggleTodo: PropTypes.func.isRequired,
-  editMode: PropTypes.objectOf(PropTypes.shape({
-    mode: PropTypes.bool.isRequired,
-    todo: PropTypes.objectOf(PropTypes.shape({
-      _id: PropTypes.number,
-      title: PropTypes.string,
-      completed: PropTypes.bool,
-    })).isRequired,
-  }).isRequired).isRequired,
-  todos: PropTypes.arrayOf(PropTypes.shape({
-    _id: PropTypes.number,
-    title: PropTypes.string,
-    completed: PropTypes.bool,
-  })).isRequired,
-  textField: PropTypes.string.isRequired,
-};
--- src/components/MaterialTextField.jsx
+++ src/components/MaterialTextField.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
 import { TextField } from 'material-ui';
-import { propTypes } from 'redux-form';
-import PropTypes from 'prop-types';
 
-const MaterialTextField = ({ label, input }) => (
+type Props = {
+  label: string,
+  input: Object,
+};
+
+const MaterialTextField = ({ label, input }: Props) => (
   <TextField label={label} {...input} margin="normal" />
 );
 export default MaterialTextField;
-
-MaterialTextField.propTypes = {
-  label: PropTypes.string.isRequired,
-  input: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    onBlur: PropTypes.func,
-    onChange: PropTypes.func,
-    onDragStart: PropTypes.func,
-    onDrop: PropTypes.func,
-    onFocus: PropTypes.func,
-    value: PropTypes.any,
-  }).isRequired,
-};
